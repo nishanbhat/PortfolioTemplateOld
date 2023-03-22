910639469636b2ce56d5b4a22a25c39fec8ab0f4
nishanbhat <rananishanbhat@gmail.com>
2023-03-22 11:48:25 +0545

    projects included

--- script.js
+++ script.js
@@ -45,14 +45,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Frontend Developer", "UI Designer"],
+    strings: ["Frontend Developer", "UI/UX Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Frontend Developer", "UI Designer"],
+    strings: ["Frontend Developer", "UI/UX Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
